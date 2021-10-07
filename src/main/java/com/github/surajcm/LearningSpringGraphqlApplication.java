dee04c3a4c25e15b02e073477bda988fcbfc4134
Suraj Muraleedharan <v-smuraleedharan@expediagroup.com>
2021-10-07 22:55:04 +0530

    first commit

---
+++ src/main/java/com/github/surajcm/LearningSpringGraphqlApplication.java
@@ -0,0 +1,13 @@
+package com.github.surajcm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringGraphqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningSpringGraphqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/surajcm/LearningSpringGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.surajcm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringGraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
